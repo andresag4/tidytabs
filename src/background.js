5ac6835c6d4cb5d72fd69898bde48bcee91dfe5d
Andres AG <andres.alvg4@gmail.com>
2026-05-31 01:06:50 -0600

    feat: wire context menu into background service worker

--- src/background.js
+++ src/background.js
@@ -1,5 +1,6 @@
 import { runTriage } from './group.js';
 import { getSettings } from './settings.js';
+import { register as registerContextMenu } from './contextmenu.js';
 
 chrome.action.onClicked.addListener(async () => {
   try {
@@ -9,3 +10,9 @@ chrome.action.onClicked.addListener(async () => {
     console.error('tidytabs: triage failed', e);
   }
 });
+
+// Register the right-click menu both on install (one-time) and on every
+// service worker startup (after restart, since menus are not persisted).
+chrome.runtime.onInstalled.addListener(() => registerContextMenu());
+chrome.runtime.onStartup.addListener(() => registerContextMenu());
+registerContextMenu();
